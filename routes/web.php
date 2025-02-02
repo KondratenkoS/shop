1275f2803e62602c2d070571bd13fe20edf6d666
hell <sergeikhell@gmail.com>
2025-02-02 22:52:39 +0200

    сделан фронт AdminLTE 3, круд для категорий

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\Category\CreateController;
+use App\Http\Controllers\Category\DeleteController;
+use App\Http\Controllers\Category\EditController;
+use App\Http\Controllers\Category\IndexController;
+use App\Http\Controllers\Category\ShowController;
+use App\Http\Controllers\Category\StoreController;
+use App\Http\Controllers\Category\UpdateController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
+
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('/', IndexController::class)->name('category.index');
+    Route::get('/create', CreateController::class)->name('category.create');
+    Route::post('/', StoreController::class)->name('category.store');
+    Route::get('/{category}/edit', EditController::class)->name('category.edit');
+    Route::get('/{category}', ShowController::class)->name('category.show');
+    Route::patch('/{category}', UpdateController::class)->name('category.update');
+    Route::delete('/{category}', DeleteController::class)->name('category.delete');
 });
